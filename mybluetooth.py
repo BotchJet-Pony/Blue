4df1c6c6b2d9bae81317557c8734b03dc2014f2c
BotchJet-Pony <74597410+BotchJet-Pony@users.noreply.github.com>
2022-04-21 21:29:43 -0300

    Add files via upload

---
+++ mybluetooth.py
@@ -0,0 +1,49 @@
+from opcode import hascompare
+import requests
+import socket 
+import random
+import hashlib
+import secrets
+import bluetooth
+
+# part 1 
+devices_discovery = bluetooth.DeviceDiscoverer()
+print()
+
+bluetooth_socket = bluetooth.BluetoothSocket()
+print("finally_address")
+
+nearby_devices = bluetooth.discover_devices(lookup_names=True, lookup_class=True)
+
+look_name = bluetooth.lookup_name("04:CB:88:E3:18:18")
+zone_id = "take the zone identifier"
+list_of_devices = []
+sum_number1 = secrets.token_bytes()
+sum_number2 = secrets.token_bytes()
+list_of_devices.append(nearby_devices)
+
+# ClassName.Method 
+
+class Discovery:
+
+    lenght_packet = (1,3,5)
+
+    def discover(self):
+        return 
+    
+
+monster = Discovery()
+
+
+# create encrypted files part 2
+hash = hashlib.sha256()
+hash.update(b"askdlakdlaksdl")
+hash.update(b"  Bill Card Slim ")
+xml_write = str(hash.digest())
+with open("file1.xml", "a+") as file:  
+    file.writelines(xml_write)
+
+
+
+
+# part 3 posting in some website
